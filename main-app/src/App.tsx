82b1e67a3c1a9b12a7a1a2df12edb805dbf8dfcc
hermanke <hermanke@tencent.com>
2021-05-22 12:09:03 +0800

    test: 动态添加没有内容的style标签

--- main-app/src/App.tsx
+++ main-app/src/App.tsx
@@ -6,6 +6,18 @@ import AppMenu from "./menu/index";
 import AppRoute from "./routes";
 
 const App: FC = () => {
+  // useEffect(() => {
+  //   Object.defineProperty(window, "test", {
+  //     get: function () {
+  //       return 1;
+  //     },
+  //     set: function (value) {
+  //       this.test = value;
+  //     },
+  //     configurable: false,
+  //     enumerable: true,
+  //   });
+  // }, []);
   return (
     <Router>
       <div className={classes.container}>
--- micro-app-react/src/App.tsx
+++ micro-app-react/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useEffect } from "react";
-// import { message } from "antd";
+import { message } from "antd";
 
 import classes from "./App.less";
 
@@ -10,7 +10,26 @@ interface AppProps {
 const App: FC<AppProps> = (props) => {
   const { data } = props;
   useEffect(() => {
-    // message.error("error");
+    message.error("error");
+    // (window as CommonObject).test = 100;
+    // console.log((window as CommonObject).test);
+  }, []);
+
+  // 测试动态添加style
+  useEffect(() => {
+    const ele = document.querySelector("#test-style");
+    if (ele) return;
+    const style = document.createElement("style");
+    style.type = "text/css";
+    style.id = "test-style";
+    // const innerHtml = "span{color:red}";
+    // style.innerHTML = innerHtml;
+
+    document.querySelector("head")?.appendChild(style);
+
+    // 直接设置CSSRules
+    style.sheet?.insertRule("span{color:red}", 0);
+    style.textContent = "";
   }, []);
 
   return (
